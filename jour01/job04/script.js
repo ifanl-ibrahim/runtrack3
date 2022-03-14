31ee71c72d13698bf68311ace4423ccd1b22680c
ifanl-ibrahim <ifanl.ibrahim@laplateforme.io>
2022-03-14 14:43:17 +0100

    job 04 / boucle pour voir si une année est bissextile

---
+++ jour01/job04/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function bisextile(année)
+{
+    var année =  prompt('Entrer l\'année ');
+    
+    if(année % 4 == 0 )
+    {
+        alert(true);
+    }else{
+        alert(false);
+    }   
+    console.log(année);
+}
+
+bisextile();
