b112d089761a9fd312f7ec18a8849ceb70ce5f8b
ifanl-ibrahim <ifanl.ibrahim@laplateforme.io>
2022-03-21 23:25:13 +0100

    jour06

---
+++ jour06/job04/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Runtrack3</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <!-- menu pc -->
+            <ul>
+                <p>Acceuil</p>
+                <p>Présentation</p>
+                <p>Contact</p>
+            </ul>
+        </nav>
+
+        <nav class="drop">
+            <!-- menu mobil  -->
+            <button></button>
+            <div class="container-button">
+                <p>Acceuil</p>
+                <p>Présentation</p>
+                <p>Contact</p>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
